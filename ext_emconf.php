1eb4352f68ae3ed41764d2189e3f94947cfe3af7
Stefan Froemken <froemken@gmail.com>
2017-09-22 10:17:32 +0200

    Bugfix in Fluid Variable subheader

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'typo3' => '6.2.0-8.99.99',
-            'fs_code_snippet' => '1.4.0'
+            'fs_code_snippet' => '1.8.*'
         ),
         'conflicts' => array(
         ),
