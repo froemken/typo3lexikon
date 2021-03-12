e902bf6b7db572fb76f85624b58080f1e9bf45b2
Stefan Froemken <froemken@gmail.com>
2021-03-12 23:22:25 +0000

    Add LocalConfiguration and PackageStates

---
+++ public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,155 @@
+<?php
+return [
+    'BE' => [
+        'compressionLevel' => '9',
+        'debug' => false,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$argon2i$v=19$m=16384,t=16,p=2$NXdiTTdTbzE0dGNPTU4ydA$ltzKsw45OTphok+MxdJAxq9vKjr20UBlxRQmVsvLCb0',
+        'lockSSL' => true,
+        'lockSSLPort' => 443,
+        'loginSecurityLevel' => 'normal',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+        'warning_email_addr' => 'froemken@gmail.com',
+        'warning_mode' => 2,
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'db226943_774',
+                'driver' => 'mysqli',
+                'host' => '127.0.0.3',
+                'password' => ':4NmrqexBgz5',
+                'port' => 3306,
+                'tableoptions' => [
+                    'charset' => 'utf8',
+                    'collate' => 'utf8_general_ci',
+                ],
+                'user' => 'db226943_774',
+            ],
+        ],
+    ],
+    'EXT' => [],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'scheduler' => [
+            'maxLifetime' => '1440',
+            'showSampleTasks' => '1',
+        ],
+        'solr' => [
+            'allowSelfSignedCertificates' => '0',
+            'useConfigurationFromClosestTemplate' => '0',
+            'useConfigurationMonitorTables' => '',
+            'useConfigurationTrackRecordsOutsideSiteroot' => '1',
+        ],
+    ],
+    'FE' => [
+        'compressionLevel' => 9,
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'enable_mount_pids' => false,
+        'loginSecurityLevel' => 'normal',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'defaultMailFromAddress' => 'info@typo3lexikon.de',
+        'defaultMailFromName' => 'Stefan Frömken',
+        'defaultMailReplyToAddress' => 'info@typo3lexikon.de',
+        'defaultMailReplyToName' => 'Stefan Frömken',
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/sbin/sendmail -t -i ',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'pagesection' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+            ],
+        ],
+        'ddmmyy' => 'd.m.Y',
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '85ba9f5bc7d2509971f32e594b4917873a90b8143a5d9528da18e76b215882a3e090ec73f8af9279ec66980c51d3e97d',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'TypoScript.strictSyntax' => true,
+            'redirects.hitCount' => false,
+            'security.frontend.keepSessionDataOnLogout' => false,
+            'simplifiedControllerActionDispatching' => false,
+            'unifiedPageTranslationHandling' => true,
+        ],
+        'loginCopyrightWarrantyProvider' => 'Stefan Frömken',
+        'loginCopyrightWarrantyURL' => 'https://www.typo3lexikon.de',
+        'phpTimeZone' => 'Europe/Berlin',
+        'sitename' => 'Stefans TYPO3 Seite',
+        'systemLocale' => 'de_DE.UTF-8',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
