f357f2d3de44cff0ea49afb41946f67de828ca45
Stefan Froemken <froemken@gmail.com>
2022-06-09 21:57:10 +0200

    Add site config for ionos

--- Source/Scripts/Typo3Context.php
+++ Source/Scripts/Typo3Context.php
@@ -8,8 +8,8 @@ $context = 'Production';
 if (array_key_exists('HTTP_HOST', $_SERVER)) {
     if ($_SERVER['HTTP_HOST'] === 'typo3lexikon.home') {
         $context = 'Development/DDevHome';
-    } elseif ($_SERVER['HTTP_HOST'] === 'typo3lexikon.typo3retter.de') {
-        $context = 'Production/Netcup';
+    } elseif ($_SERVER['HTTP_HOST'] === 'home-5008552505.app-ionos.space') {
+        $context = 'Production/Ionos';
     } elseif (preg_match('@^.*\.ddev\.site$@', $_SERVER['HTTP_HOST'])) {
         $context = 'Development/DDev';
     }
