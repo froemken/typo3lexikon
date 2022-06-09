265c3644b8f9506418c1d6668790729157cfd093
Stefan Froemken <froemken@gmail.com>
2022-06-09 19:59:20 +0200

    Add more env vars for DB config

--- public/typo3conf/AdditionalConfiguration.example.php
+++ public/typo3conf/AdditionalConfiguration.example.php
@@ -12,13 +12,17 @@ if (getenv('DEBUG')) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 0;
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['driver'] = getenv('DB_CONNECTION');
-$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = getenv('DB_DATABASE');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['driver'] = getenv('DB_DRIVER');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = getenv('DB_NAME');
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = getenv('DB_HOST');
-$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = getenv('DB_PASSWORD');
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['port'] = getenv('DB_PORT');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['charset'] = getenv('DB_CHARSET');
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = getenv('DB_USERNAME');
-
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = getenv('DB_PASSWORD');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['tableoptions'] = [
+    'charset' => getenv('DB_TABLE_CHARSET'),
+    'collate' => getenv('DB_TABLE_COLLATE')
+];
 
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor'] = 'ImageMagick';
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_path'] = '/usr/bin/';
