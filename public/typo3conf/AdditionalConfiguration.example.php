de4d708359914fe3650e3f1ecd086546271ed223
Stefan Froemken <froemken@gmail.com>
2022-06-09 19:23:02 +0200

    Use GitHub secret to configure debug mode

--- public/typo3conf/AdditionalConfiguration.example.php
+++ public/typo3conf/AdditionalConfiguration.example.php
@@ -1,6 +1,16 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = false;
+if (getenv('DEBUG')) {
+    $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = true;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
+} else {
+    $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = false;
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 0;
+}
 
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['driver'] = getenv('DB_CONNECTION');
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = getenv('DB_DATABASE');
@@ -9,13 +19,9 @@ $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = getenv
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['port'] = getenv('DB_PORT');
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = getenv('DB_USERNAME');
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
 
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor'] = 'ImageMagick';
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_path'] = '/usr/bin/';
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_path_lzw'] = '/usr/bin/';
 
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = 'smtp';
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 0;
