8288d9495fe1053d4495f0d4733fda525a097bec
Stefan Froemken <froemken@gmail.com>
2022-06-09 19:04:02 +0200

    Fill AdditionalConfiguration with ENV vars

--- public/typo3conf/AdditionalConfiguration.example.php
+++ public/typo3conf/AdditionalConfiguration.example.php
@@ -1,76 +1,21 @@
 <?php
 
-if (getenv('IS_DDEV_PROJECT') == 'true') {
-    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
-        $GLOBALS['TYPO3_CONF_VARS'],
-        [
-            'BE' => [
-                'debug' => true
-            ],
-            'DB' => [
-                'Connections' => [
-                    'Default' => [
-                        'dbname' => 'db',
-                        'host' => 'db',
-                        'password' => 'db',
-                        'port' => '3306',
-                        'user' => 'db',
-                    ],
-                ],
-            ],
-            'FE' => [
-                'debug' => true
-            ],
-            // This GFX configuration allows processing by installed ImageMagick 6
-            'GFX' => [
-                'processor' => 'ImageMagick',
-                'processor_path' => '/usr/bin/',
-                'processor_path_lzw' => '/usr/bin/',
-            ],
-            // This mail configuration sends all emails to mailhog
-            'MAIL' => [
-                'transport' => 'smtp',
-                'transport_smtp_server' => 'localhost:1025',
-            ],
-            'SYS' => [
-                'trustedHostsPattern' => '.*.*',
-                'devIPmask' => '*',
-                'displayErrors' => 1,
-            ],
-        ]
-    );
-}
+$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = false;
 
-call_user_func(function () {
-    $apcuBackend = [
-        'backend' => \TYPO3\CMS\Core\Cache\Backend\ApcuBackend::class,
-        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
-        'group' => 'system',
-        'options' => [
-            'defaultLifetime' => 0
-        ]
-    ];
-    $redisBackend = [
-        'backend' => \TYPO3\CMS\Core\Cache\Backend\RedisBackend::class,
-        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
-        'groups' => ['pages'],
-        'options' => [
-            'hostname' => 'redis',
-            'port' => 6379,
-            'persistentConnection' => false,
-            'database' => 3,
-            'compression' => false,
-            'compressionLevel' => -1
-        ]
-    ];
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['driver'] = getenv('DB_CONNECTION');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = getenv('DB_DATABASE');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = getenv('DB_HOST');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = getenv('DB_PASSWORD');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['port'] = getenv('DB_PORT');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = getenv('DB_USERNAME');
 
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['hash'] = $redisBackend;
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['pages'] = $redisBackend;
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['pagesection'] = $redisBackend;
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['rootline'] = $redisBackend;
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['imagesizes'] = $redisBackend;
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['imagesizes']['group'] = 'lowlevel';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
 
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extbase'] = $apcuBackend;
-});
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['processor'] = 'ImageMagick';
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_path'] = '/usr/bin/';
+$GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_path_lzw'] = '/usr/bin/';
 
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = 'smtp';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 0;
