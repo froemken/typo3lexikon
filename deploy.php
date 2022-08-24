756a2c3c364dffe1463020e393065dc84f504c20
Stefan Froemken <froemken@gmail.com>
2022-08-24 22:14:29 +0200

    Update typo3 webroot to public

--- deploy.php
+++ deploy.php
@@ -3,8 +3,10 @@ namespace Deployer;
 
 require 'recipe/typo3.php';
 
-// Config
+// DocumentRoot / WebRoot for the TYPO3 installation
+set('typo3_webroot', 'public');
 
+// Config
 set('repository', 'https://github.com/froemken/typo3lexikon');
 
 add('shared_files', []);
@@ -12,12 +14,10 @@ add('shared_dirs', []);
 add('writable_dirs', []);
 
 // Hosts
-
 host('sfroemken.de')
     ->set('remote_user', 'froemken')
     ->set('deploy_path', '~/httpdocs/typo3lexikon.sfroemken.de')
     ->set('ssh_multiplexing', false); // GitHub action seems to use an old OpenSSH version
 
 // Hooks
-
 after('deploy:failed', 'deploy:unlock');
