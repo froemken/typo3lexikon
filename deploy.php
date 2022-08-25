ffe8d9d1482cf1854d2770db93b7ca67172cd552
Stefan Froemken <froemken@gmail.com>
2022-08-25 18:39:35 +0200

    Upload AdditionalConfiguration while deploy

--- deploy.php
+++ deploy.php
@@ -3,21 +3,39 @@ namespace Deployer;
 
 require 'recipe/typo3.php';
 
+if (
+    array_key_exists('HTTP_HOST', $_SERVER)
+    && preg_match('@^.*\.ddev\.site$@', $_SERVER['HTTP_HOST'])
+) {
+    echo 'Do not deploy locally. Use GIT push and wait for Github actions';
+    exit;
+}
+
 // DocumentRoot / WebRoot for the TYPO3 installation
 set('typo3_webroot', 'public');
 
 // Config
-set('repository', 'https://github.com/froemken/typo3lexikon');
-
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
+task('deploy:uploadConfig', function () {
+    upload(
+        'public/typo3conf/AdditionalConfiguration.php',
+        'public/typo3conf/AdditionalConfiguration.php'
+    );
+});
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'deploy:publish',
+]);
+
 // Hosts
 host('sfroemken.de')
     ->set('remote_user', 'froemken')
-    ->set('deploy_path', '~/httpdocs/typo3lexikon.sfroemken.de')
-    ->set('ssh_multiplexing', false); // GitHub action seems to use an old OpenSSH version
+    ->set('deploy_path', '~/httpdocs/typo3lexikon.sfroemken.de');
 
 // Hooks
 after('deploy:failed', 'deploy:unlock');
