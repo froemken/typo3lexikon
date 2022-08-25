5cc8c39366c932b78355b246ea92be1cd350eae4
Stefan Froemken <froemken@gmail.com>
2022-08-25 20:19:03 +0200

    Use release_path to upload config file

--- deploy.php
+++ deploy.php
@@ -6,9 +6,13 @@ require 'recipe/typo3.php';
 // DocumentRoot / WebRoot for the TYPO3 installation
 set('typo3_webroot', 'public');
 
+// Hosts
+host('sfroemken.de')
+    ->set('remote_user', 'froemken')
+    ->set('deploy_path', '~/httpdocs/typo3lexikon.sfroemken.de');
+
 // Config
 set('repository', 'https://github.com/froemken/typo3lexikon');
-
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
@@ -16,16 +20,11 @@ add('writable_dirs', []);
 task('deploy:uploadConfig', function () {
     upload(
         'public/typo3conf/AdditionalConfiguration.php',
-        '~/httpdocs/typo3lexikon.sfroemken.de/public/typo3conf/AdditionalConfiguration.php'
+        get('release_path') . '/public/typo3conf/AdditionalConfiguration.php'
     );
 });
 
 after('deploy', 'deploy:uploadConfig');
-
-// Hosts
-host('sfroemken.de')
-    ->set('remote_user', 'froemken')
-    ->set('deploy_path', '~/httpdocs/typo3lexikon.sfroemken.de');
 
 // Hooks
 after('deploy:failed', 'deploy:unlock');
