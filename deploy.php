abb2297a2722d57b6ad75c324baae305f834567d
Stefan Froemken <froemken@gmail.com>
2023-03-25 15:09:19 +0100

    copy file with scp

--- deploy.php
+++ deploy.php
@@ -18,10 +18,9 @@ add('writable_dirs', []);
 
 // Remote tasks
 task('deploy:postProcess', function () {
-    upload(
-        'public/typo3conf/AdditionalConfiguration.php',
-        get('release_path') . '/public/typo3conf/AdditionalConfiguration.php'
-    );
+    // upload() does not work because of missing rsync at target host
+    run('scp public/typo3conf/AdditionalConfiguration.php ' . $this->get('hostname') . ':' . get('release_path') . '/public/typo3conf/AdditionalConfiguration.php');
+
     run('cd ' . get('release_path') . ' && vendor/bin/typo3cms database:updateschema "*.add,*.change"');
     run('cd ' . get('release_path') . ' && vendor/bin/typo3cms language:update');
     run('cd ' . get('release_path') . ' && vendor/bin/typo3cms cache:flush');
